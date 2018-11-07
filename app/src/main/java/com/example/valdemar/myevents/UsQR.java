c6324b8a9f350b0e265d68359ff3e0b1d2c68f1a
unknown <amigo_diablito@hotmail.com>
2018-11-07 11:31:42 -0400

    QR con Control y Modelo Eventos

--- app/src/main/java/com/example/valdemar/myevents/UsQR.java
+++ app/src/main/java/com/example/valdemar/myevents/UsQR.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -17,24 +18,37 @@ import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 public class UsQR extends Fragment {
     private View V;
-    private TextView NOM;
+    private TextView NOM,AP,CI,PROF,CAR,INS;
     private Button CREA;
     private ImageView IMG;
+    private String DATOS;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         V = inflater.inflate(R.layout.fragment_us_qr, container, false);
         NOM = (TextView)V.findViewById(R.id.TVENOM);
+        AP = (TextView)V.findViewById(R.id.TVEAPE);
+        CI = (TextView)V.findViewById(R.id.TVECI);
+        CAR = (TextView)V.findViewById(R.id.TVECAR);
+        INS = (TextView)V.findViewById(R.id.TVEINS);
+        PROF = (TextView)V.findViewById(R.id.TVEPROF);
+
         CREA = (Button)V.findViewById(R.id.button);
         IMG = (ImageView)V.findViewById(R.id.imageView3);
         CREA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Text =NOM.getText().toString();
-                if(Text!=null){
+                String nom = NOM.getText().toString();
+                String ap = AP.getText().toString();
+                String ci = CI.getText().toString();
+                String car = CAR.getText().toString();
+                String ins = INS.getText().toString();
+                String prof = PROF.getText().toString();
+                DATOS = nom+"; "+ap+"; "+ci+"; "+car+"; "+ins+"; "+prof;
+                if(nom!=null){
                     try {
                         MultiFormatWriter multi = new MultiFormatWriter();
-                        BitMatrix Bma = multi.encode(Text, BarcodeFormat.QR_CODE,500,500);
+                        BitMatrix Bma = multi.encode(DATOS, BarcodeFormat.QR_CODE,600,600);
                         BarcodeEncoder barcodeEncoder = new BarcodeEncoder ();
                         Bitmap bitmap = barcodeEncoder.createBitmap(Bma);
                         IMG.setImageBitmap (bitmap);
@@ -42,9 +56,16 @@ public class UsQR extends Fragment {
                         e.printStackTrace();
                     }
                 }
+                Toast.makeText(getContext(),"Datos: "+DATOS, Toast.LENGTH_SHORT).show();
+
             }
         });
-
+        LlenarDatos();
         return  V;
     }
+    private void LlenarDatos() {
+        Toast.makeText(getContext(),"En la Funcion ", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
