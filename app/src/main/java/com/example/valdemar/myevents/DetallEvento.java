f5cb117359cd8fc537902d1e58f30cc2ddcb5e17
unknown <amigo_diablito@hotmail.com>
2018-11-08 07:35:51 -0400

    Destalle del Evento con Diseño :3

--- app/src/main/java/com/example/valdemar/myevents/DetallEvento.java
+++ app/src/main/java/com/example/valdemar/myevents/DetallEvento.java
@@ -1,23 +1,36 @@
 package com.example.valdemar.myevents;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class DetallEvento extends AppCompatActivity {
     private TextView nom,fechai,fechaf,horai,horaf,desc,cost;
+    private Typeface Letra;
+    private String fuente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fuente ="fuentes/Charline.otf";
         setContentView(R.layout.activity_detall_evento);
         nom=(TextView)findViewById(R.id.NOM);
-        fechai=(TextView)findViewById(R.id.textView4);
-        fechaf=(TextView)findViewById(R.id.textView6);
-        horai=(TextView)findViewById(R.id.textView7);
-        horaf=(TextView)findViewById(R.id.textView8);
-        desc=(TextView)findViewById(R.id.textView9);
-        cost=(TextView)findViewById(R.id.textView10);
+        fechai=(TextView)findViewById(R.id.FEI);
+        fechaf=(TextView)findViewById(R.id.FEF);
+        horai=(TextView)findViewById(R.id.HOI);
+        horaf=(TextView)findViewById(R.id.HOF);
+        desc=(TextView)findViewById(R.id.DES);
+        cost=(TextView)findViewById(R.id.COS);
+
+        this.Letra = Typeface.createFromAsset(getAssets(),fuente);
+        nom.setTypeface(Letra);
+        fechai.setTypeface(Letra);
+        fechaf.setTypeface(Letra);
+        horai.setTypeface(Letra);
+        horaf.setTypeface(Letra);
+        desc.setTypeface(Letra);
+        cost.setTypeface(Letra);
 
         Intent detalle = getIntent();
         Bundle B = getIntent().getExtras();
