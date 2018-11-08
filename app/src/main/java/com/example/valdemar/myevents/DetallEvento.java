75cf705b200b7e42c0ccd89204b15d5f509a9f6e
unknown <amigo_diablito@hotmail.com>
2018-11-08 15:22:11 -0400

    letras para el detalle del eveto

--- app/src/main/java/com/example/valdemar/myevents/DetallEvento.java
+++ app/src/main/java/com/example/valdemar/myevents/DetallEvento.java
@@ -2,18 +2,17 @@ package com.example.valdemar.myevents;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class DetallEvento extends AppCompatActivity {
-    private TextView nom,fechai,fechaf,horai,horaf,desc,cost;
+    private TextView nom,fechai,fechaf,horai,horaf,desc,cost, Fechas,lf,lho,lh,ldes,lcos,funi;
     private Typeface Letra;
     private String fuente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        fuente ="fuentes/Charline.otf";
         setContentView(R.layout.activity_detall_evento);
         nom=(TextView)findViewById(R.id.NOM);
         fechai=(TextView)findViewById(R.id.FEI);
@@ -22,7 +21,41 @@ public class DetallEvento extends AppCompatActivity {
         horaf=(TextView)findViewById(R.id.HOF);
         desc=(TextView)findViewById(R.id.DES);
         cost=(TextView)findViewById(R.id.COS);
+        Fechas=(TextView)findViewById(R.id.Lfecha);
+        lf=(TextView)findViewById(R.id.Lf);
+        lh=(TextView)findViewById(R.id.Lh);
+        ldes=(TextView)findViewById(R.id.Ldes);
+        lcos=(TextView)findViewById(R.id.LCos);
+        lho=(TextView)findViewById(R.id.Lho);
+        funi=(TextView)findViewById(R.id.FUni);
+        cambio();
+        Intent detalle = getIntent();
+        Bundle B = getIntent().getExtras();
+        if(B != null){
+            if(B.getString("fei").toString().compareTo(B.getString("fef").toString())==0){
+                //Toast.makeText(this, "son iguales", Toast.LENGTH_SHORT).show();
+                nom.setText(B.getString("nom"));
+                funi.setText(B.getString("fei"));
+                horai.setText(B.getString("hoi"));
+                horaf.setText(B.getString("hof"));
+                desc.setText(B.getString("des"));
+                cost.setText(B.getString("cos"));
+            }else{
+                //Toast.makeText(this, "no son iguales", Toast.LENGTH_SHORT).show();
+                fechai.setText(B.getString("fei"));
+                lf.setText("a");
+                fechaf.setText(B.getString("fef"));
+                nom.setText(B.getString("nom"));
+                horai.setText(B.getString("hoi"));
+                horaf.setText(B.getString("hof"));
+                desc.setText(B.getString("des"));
+                cost.setText(B.getString("cos"));
+            }
+        }
+    }
 
+    private void cambio() {
+        fuente ="fuentes/PhItalic.ttf";
         this.Letra = Typeface.createFromAsset(getAssets(),fuente);
         nom.setTypeface(Letra);
         fechai.setTypeface(Letra);
@@ -31,17 +64,13 @@ public class DetallEvento extends AppCompatActivity {
         horaf.setTypeface(Letra);
         desc.setTypeface(Letra);
         cost.setTypeface(Letra);
-
-        Intent detalle = getIntent();
-        Bundle B = getIntent().getExtras();
-        if(B != null){
-            nom.setText(B.getString("nom"));
-            fechai.setText(B.getString("fei"));
-            fechaf.setText(B.getString("fef"));
-            horai.setText(B.getString("hoi"));
-            horaf.setText(B.getString("hof"));
-            desc.setText(B.getString("des"));
-            cost.setText(B.getString("cos"));
-        }
+        Fechas.setTypeface(Letra);
+        lf.setTypeface(Letra);
+        lh.setTypeface(Letra);
+        ldes.setTypeface(Letra);
+        lcos.setTypeface(Letra);
+        lho.setTypeface(Letra);
+        funi.setTypeface(Letra);
     }
+
 }
